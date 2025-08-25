85ed8d66a6bb2b3c5bc18a095eb4145965fa05cf
Brijesh Balamurugan <brijeshh@Brijeshs-MacBook-Pro.local>
2025-08-25 17:40:06 +0530

    TW-352

---
+++ terraform-project-setup/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "The AWS region to deploy"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "aws_profile" {
+  description = "AWS CLI profile name"
+  type        = string
+  default     = "default"
+}
